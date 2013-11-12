9b217528f6c8042a59f717519fd5bdce39a1a925
João Martins <joaocmfcm@gmail.com>
2013-11-12 03:55:06 +0000

    v1.0

--- content.js
+++ content.js
@@ -11,10 +11,9 @@ $('.artists .artist').each(function(){
 fullTrackName = fullTrackName.concat(artists).concat('- ').concat($('.trackName.txt-uppercase').text());
 
 //create the search query for youtube
-var searchQuery = "http://www.youtube.com/results?search_query="+fullTrackName+"&sm=3"
+var searchQuery = "http://www.youtube.com/results?search_query=".concat(fullTrackName).concat("&sm=3");
 
-//reloads the window. this is actually needed in order 
-//to refresh page content because the information of the track would not change after the first usage
+//reloads the window. this is actually needed in order to refresh the page content. the information of the track would not change after the first usage
 location.reload();
-var win=window.open(searchQuery, 'BeatportToYoutube');
+var win = window.open(searchQuery, 'BeatportToYoutube');
 win.focus();
