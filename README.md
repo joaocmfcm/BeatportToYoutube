1ba9df35f1a0da68c85ad2dd965ce06a579b6378
João Martins <joaocmfcm@gmail.com>
2015-07-13 21:14:29 +0100

    Added support to the new Beatport Pro website. Fixed an old issue where the Beatport page had to reload every time the extension was used.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 BeatportToYoutube
 =================
 
-This is a simple Google Chrome extension to search on Youtube for the current song playing on Beatport.
+This is a simple Google Chrome extension to search on Youtube for the current song playing on your Beatport Pro.
 
 How to use it
 =================
-Type "chrome://extensions/" on Chrome's address bar.
-Drag and drop the "BeatportToYoutube.crx" file to Chrome.
-Click on a red "b" icon on Chrome's extension icons while playing a song on Beatport.
+Type "chrome://extensions/" on Chrome's address bar or go to the Chrome settings and then click on the Extensions panel.
+Drag and drop the "BeatportToYoutube.crx" file to Chrome Extensions panel.
+Click on the red "b" icon on Chrome's extension icons while playing a song on Beatport Pro.
--- content.js
+++ content.js
@@ -1,19 +1,22 @@
-var fullTrackName = "";
-var artists = "";
+var fullTrackName = '';
+var trackName = '';
+var mix = '';
+var artists = '';
 
 //get all artists
-$('.artists .artist').each(function(){
-    artists = artists.concat($(this).text());
-    artists = artists.concat(' ');
-});
+artists = $('.player-current-track-container .player-track-name-artist.player-track-name-artist-standard .track-artist').text();
 
-//get trackname
-fullTrackName = fullTrackName.concat(artists).concat('- ').concat($('.trackName.txt-uppercase').text());
+//get track name
+trackName = $('.player-current-track-container .player-track-name-artist.player-track-name-artist-standard .track-title .primary-title').text();
+
+//get mix name 
+mix = $('.player-current-track-container .player-track-name-artist.player-track-name-artist-standard .track-title .remixed').text();
+
+//sets the full track name
+fullTrackName = artists + ' - ' + trackName + ' ' + mix;
 
 //create the search query for youtube
-var searchQuery = "http://www.youtube.com/results?search_query=".concat(fullTrackName).concat("&sm=3");
+var searchQuery = 'http://www.youtube.com/results?search_query='.concat(fullTrackName).concat('&sm=3');
 
-//reloads the window. this is actually needed in order to refresh the page content. the information of the track would not change after the first usage
-location.reload();
 var win = window.open(searchQuery, 'BeatportToYoutube');
 win.focus();
